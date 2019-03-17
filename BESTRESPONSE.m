c9bb885f787b685ff7650710d852b422390038c2
numerology <jxzheng03@gmail.com>
2019-03-17 17:20:45 -0500

    Update BESTRESPONSE.m

--- BESTRESPONSE.m
+++ BESTRESPONSE.m
@@ -1,5 +1,6 @@
 function [userRates, userFraction, btd] = BESTRESPONSE(NetSettings, ...
     OpSettings, capacityPerUser, bs)
-
+% Sharing criterion where each tenant seeks to maximize their own utility
+% function unilaterally, which is defined by a weighted sum of log rate.
 end
 

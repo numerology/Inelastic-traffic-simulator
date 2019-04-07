d6b3c8278360427e47f7df9477eee13ee4890296
Jiaxiao Zheng <jxzheng@c506-031.stampede2.tacc.utexas.edu>
2019-04-07 11:07:37 -0500

    Update from TACC, added a new figure about protection.

--- protection_script.m
+++ protection_script.m
@@ -1,14 +1,14 @@
 clear;
 share = 0.5; % share of slice 1.
-advIntensityVec = 1:0.25:3;
-repetition = 160;
+advIntensityVec = 1:0.5:5;
+repetition = 20;
 rateVec1Equal = zeros(repetition, length(advIntensityVec));
 rateVec2Equal = zeros(repetition, length(advIntensityVec));
 rateVec1Dps = zeros(repetition, length(advIntensityVec));
 rateVec2Dps = zeros(repetition, length(advIntensityVec));
 
 gcp;
-ppm = ParforProgMon('simulating : ', length(advIntensityVec));
+
 for i = 1:length(advIntensityVec)
     for r = 1:repetition
         in(r) = Simulink.SimulationInput('SCPF_simple');
@@ -17,7 +17,7 @@ for i = 1:length(advIntensityVec)
         in(r) = in(r).setVariable('advArrivalRate', advIntensityVec(i));
         in(r) = in(r).setVariable('weightScheme', 1);
         in(r) = setModelParameter(in(r), 'SaveOutput','on', ...
-            'OutputSaveName','out', 'StiffnessThreshold', 20000, 'StopTime', '50');
+            'OutputSaveName','out', 'StiffnessThreshold', 20000, 'StopTime', '100');
     end 
     for r = (repetition + 1):(2 * repetition)
         in(r) = Simulink.SimulationInput('SCPF_simple');
@@ -26,7 +26,7 @@ for i = 1:length(advIntensityVec)
         in(r) = in(r).setVariable('advArrivalRate', advIntensityVec(i));
         in(r) = in(r).setVariable('weightScheme', 2);
         in(r) = setModelParameter(in(r), 'SaveOutput','on', ...
-            'OutputSaveName','out', 'StiffnessThreshold', 20000, 'StopTime', '50');
+            'OutputSaveName','out', 'StiffnessThreshold', 20000, 'StopTime', '100');
     end
     simOut = parsim(in, 'ShowProgress', 'on');
     
@@ -38,14 +38,15 @@ for i = 1:length(advIntensityVec)
         rateVec1Dps(r - repetition, i) = simOut(r).out{1}.Values.Data(1, 1, end);
         rateVec2Dps(r - repetition, i) = simOut(r).out{1}.Values.Data(1, 2, end);
     end
-    ppm.increment();
+    
 end
 
-%% Plots
 rate1Equal = mean(rateVec1Equal, 1);
 rate2Equal = mean(rateVec2Equal, 1);
 rate1Dps = mean(rateVec1Dps, 1);
 rate2Dps = mean(rateVec2Dps, 1);
+
+datestring = datestr(now, 30);
 
 figure()
 hold on
@@ -56,3 +57,4 @@ plot(advIntensityVec, rate2Dps, 'rx-');
 xlabel('Slice 2 traffic intensity');
 ylabel('mean service rate');
 legend('SCPF - slice 1', 'SCPF - slice 2', 'DPS - slice1', 'DPS - slice2');
+savefig(sprintf('protection-traffic-%s.fig', datestring));

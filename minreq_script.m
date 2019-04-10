597adea876a68ce6ea838e86e654afe273042679
numerology <jxzheng03@gmail.com>
2019-04-10 13:00:23 -0500

    Update minreq_script.m

--- minreq_script.m
+++ minreq_script.m
@@ -2,12 +2,12 @@
 clc, close all, clear all, gcp;
 nSlice = 3;
 
-simulationTime = 3000;
+simulationTime = 1;
 perBSLoad = 18;
 shareVec = [14/9 13/18 13/18];
-relativeRhoVec = [4/3 * perBSLoad * [2/3 1/6 1/6];
-                  4/3 * perBSLoad * [2/3 1/6 1/6];
-                  perBSLoad * [2/9 7/18 7/18]]'; % mean load distribution, V x B
+relativeRhoVec = [perBSLoad * [2/3 1/6 1/6];
+                  perBSLoad * [2/3 1/6 1/6];
+                  3 * perBSLoad * [2/9 7/18 7/18]]'; % mean load distribution, V x B
 
 nBaseStations = size(relativeRhoVec, 2);
 capacity = 1;

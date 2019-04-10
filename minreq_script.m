7d796b5cc8fbe23f14788920c912fe0d2c8069aa
Jiaxiao Zheng <jxzheng@login4.stampede2.tacc.utexas.edu>
2019-04-10 07:45:37 -0500

    Add figures from TACC

--- minreq_script.m
+++ minreq_script.m
@@ -2,7 +2,7 @@
 clc, close all, clear all, gcp;
 nSlice = 3;
 
-simulationTime = 10000;
+simulationTime = 2000;
 perBSLoad = 18;
 shareVec = [14/9 13/18 13/18];
 relativeRhoVec = [perBSLoad * [2/3 1/6 1/6];

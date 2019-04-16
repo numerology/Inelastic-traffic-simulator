0efae8b5ae7ca02588a9a3f071f8ae8b00389a47
Jiaxiao Zheng <jxzheng@login2.stampede2.tacc.utexas.edu>
2019-04-15 22:05:39 -0500

    Update simulationTime

--- minreq_script_poisson.m
+++ minreq_script_poisson.m
@@ -4,7 +4,7 @@ parpool('local', 40);
 warning('off','all');
 nSlice = 3;
 
-simulationTime = 3000;
+simulationTime = 10000;
 perBSLoad = 6;
 shareVec = [1 1 1];
 relativeRhoVec = [perBSLoad * [1/3 1/3 1/3];

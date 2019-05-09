b7dbfda46f6a6655b4e0cd686453f891d8ac5a3f
Jiaxiao Zheng <jxzheng@login2.stampede2.tacc.utexas.edu>
2019-05-09 10:30:58 -0500

    Local update

--- full_slicing_guarantee.m
+++ full_slicing_guarantee.m
@@ -6,8 +6,8 @@ warning('on','all');
 %% Set up
 nSlices = 4; % num of slices
 
-sat = 4; % U/B (use only integers...)
-simulationTime = 2000; % seconds
+sat = 20; % U/B (use only integers...)
+simulationTime = 1000; % seconds
 
 phiLevels = 1;alphas = [1, 1, 1, 1]; % legacy parameters
 warmup = 0;
